062ed187a162e1d1c0b8ed2ecbd88f06d23ad1b9
godltjsdud <4446207@naver.com>
2022-04-27 13:56:34 +0900

    url.py - urlpatterns path 수정

--- hstack/core/urls.py
+++ hstack/core/urls.py
@@ -3,14 +3,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = "core"
+app_name = "Core"
 
 urlpatterns = [
-    path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('core/create_post/', views.PostCreate.as_view()),
     path('category/<str:slug>/', views.category_page),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
-    #path("", views.uploadFile, name="uploadFile")
+    #path('/upload/', views.uploadFile, name="uploadFile"),
 ]
 
 if settings.DEBUG: 
