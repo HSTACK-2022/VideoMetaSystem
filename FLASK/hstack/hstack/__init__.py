3b0e7a7f796f6f587d81b8eedeb419457e5f2699
yeondelight <hihi5v@hansung.ac.kr>
2022-07-19 16:32:12 +0900

    [FLASK] Add Flask Proj.

---
+++ FLASK/hstack/hstack/__init__.py
@@ -0,0 +1,25 @@
+from venv import create
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, text
+
+from . import config
+from . import models
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:hstackdbadmin@localhost/hstackDB"
+    models.db.init_app(app)
+
+    # Blueprint
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
---
+++ FLASK/hstack/hstack/config.py
@@ -0,0 +1,13 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+db = {
+    'user' : 'root',
+    'password' : 'hstackdbadmin',
+    'host' : 'localhost',
+    'port' : 3306,
+    'database' : 'hstackDB'
+}
+
+DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
