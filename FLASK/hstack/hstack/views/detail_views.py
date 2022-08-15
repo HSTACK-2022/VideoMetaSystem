1956cb34591cee55229f868e888b4d8cc8d5ef18
yeondelight <hihi5v@hansung.ac.kr>
2022-08-15 21:53:28 +0900

    [FLASK] add success.html and editFile()

--- FLASK/hstack/hstack/views/detail_views.py
+++ FLASK/hstack/hstack/views/detail_views.py
@@ -8,6 +8,7 @@ from flask import send_file
 from flask import render_template
 from flask import send_from_directory
 
+from hstack.config import OS
 from hstack.models import Videopath
 from hstack.models import Metadatum
 from hstack.models import Keyword
@@ -66,6 +67,59 @@ def detailFile(pk):
         images = pptImage,
         pptPath = pptPath,
         keywords = Keyword.query.filter(keywordQ).all(),
+        metadatas = Metadatum.query.filter(Metadatum.id == pk).all(),
+        timestamps =  Timestamp.query.filter(Timestamp.id == pk).all(),
+    )
+
+@bp.route('/detail/<int:pk>/edit', methods=['POST', 'GET'])
+def editFile(pk):
+    if request.method == "POST":
+        sysKEList = request.POST.getlist("sysKEList")
+        sysKCList = request.POST.getlist("sysKCList")
+
+        newUserKEList = request.POST.getlist("newUserKEList")
+        newUserKCList = request.POST.getlist("newUserKCList")
+        userKEList = request.POST.getlist("userKEList")
+        userKCList = request.POST.getlist("userKCList")
+
+        
+        print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+        print(sysKEList)
+        print(sysKCList)
+        print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+
+        for i in range(len(sysKEList)):
+            Keyword.query.filter(id = pk).filter(and_(Keyword.keyword == sysKCList[i], Keyword.sysdef == 1)).update(expose=sysKEList[i])
+        for i in range(len(userKEList)):
+            Keyword.query.filter(id = pk).filter(and_(Keyword.keyword == userKCList[i], Keyword.sysdef == 0)).update(expose=userKEList[i])
+        for i in range(len(newUserKCList)):
+            Keyword.query.create(
+                id = Videopath.query.filter(Videopath.id == pk).first(),
+                keyword = newUserKCList[i],
+                expose = newUserKEList[i],
+                sysdef = 0
+            )
+    
+    videoPath = Videopath.query.filter(Videopath.id == pk).first().videoAddr 
+    textPath = Videopath.query.filter(Videopath.id == pk).first().textAddr.split("hstack\\")[1]
+
+    try:
+        with open(textPath, 'r', encoding='UTF-8-sig') as f:
+            scripts = f.readlines()
+    except FileNotFoundError as err:
+        print(err)
+        scripts = []
+
+    # 이미지 받아오기
+    pptImage = makePPT.getPPTImage(videoPath)
+
+    return render_template( '/success.html',
+        pk = pk,
+        videoaddr = videoPath,
+        scripts = scripts,
+        images = pptImage,
+        keywords = Keyword.query.filter(and_(Keyword.id == pk, Keyword.sysdef == 1)),
+        userkeywords = Keyword.query.filter(and_(Keyword.id == pk, Keyword.sysdef == 0)),
         metadatas = Metadatum.query.filter(Metadatum.id == pk).all(),
         timestamps =  Timestamp.query.filter(Timestamp.id == pk).all(),
     )
