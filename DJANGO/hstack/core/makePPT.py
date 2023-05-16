9a045689179ccea4391549e0d55642e6bd6fe851
minzix <126869805+minzix@users.noreply.github.com>
2023-05-16 21:48:40 +0900

    Update makePPT.py
    I added convertPPTtoPDF function to give the users a choice between PPT file and PDF file. Thankyou in advance for reviewing my suggestion about adding a new function in your project.

--- DJANGO/hstack/core/makePPT.py
+++ DJANGO/hstack/core/makePPT.py
@@ -1,15 +1,23 @@
 import os
 import platform
+import tempfile
 
 from . import models
 from pptx import Presentation
 from pptx.util import Inches
+from fpdf import FPDF
 
 
 # 상수 설정
 OS = platform.system()
 
-def getPPTFile(videoId):
+def convertPPTtoPDF(pptPath):                                   #필요에 따라 PPT 파일 경로를 매개변수로 받고, PPT를 열어 PDF로 저장한 뒤, PDF 파일 경로를 반환
+    pdfPath = os.path.splitext(pptPath)[0] + '.pdf'
+    ppt = Presentation(pptPath)
+    ppt.save(pdfPath)
+    return pdfPath
+
+def getPPTFile(videoId, convert_to_pdf=False):
     pptFile = Presentation()
     videoPathObj = models.Videopath.objects.get(id=videoId)     #DB에서 videoId에 해당하는 객체를 가져옴
     imageFilePath = videoPathObj.imageaddr                      #DB에서 imageAddr 추출
@@ -33,12 +41,15 @@ def getPPTFile(videoId):
             slide = pptFile.slides.add_slide(slide_layout)
             slide.shapes.add_picture(imagePath + image, 0, 0, pptFile.slide_width, pptFile.slide_height)
     
-
-    if OS == "Windows" : 
+    if OS == "Windows": 
         pptPathRel = "../../../media/Uploaded/Image/" + imageFilePath.split('Image\\')[1].replace("\\", "/") + "/" + title + '.pptx'
-    else : 
+    else: 
         pptPathRel = "../../../media/Uploaded/Image/" + imageFilePath.split('Image/')[1] + "/" + title + ".pptx"
 
     pptFile.save(imagePath + title + '.pptx')
+
+    if convert_to_pdf:
+        pdfPath = convertPPTtoPDF(imagePath + title + '.pptx')
+        return pptPathRel, pdfPath
 
     return pptPathRel
